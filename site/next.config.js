cdafd9665df398d89b4806baa69f6fe7b78a693c
NeoInBoots <135618791+NeoInBoots@users.noreply.github.com>
2025-09-26 07:28:17 +0000

    modified:   site/next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -6,9 +6,9 @@ const nextConfig = {
   env: {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
+    icon: 'https://github.com/NeoInBoots/kasmregistrie/img/logo.svg',
     listUrl: 'https://github.com/NeoInBoots/kasmregistrie',
-    contactUrl: 'https://kasmweb.com/support',
+    contactUrl: 'https://github.com/NeoInBoots/kasmregistrie/support',
   },
   reactStrictMode: true,
   basePath: '/kasm-registry/1.0',
